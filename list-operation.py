03d4981be2b3d60bf5c1a47f5eb3029afd291be0
ssdharma <ssdharma@ncsu.edu>
2016-12-23 22:24:23 +0530

    create list-operations

---
+++ list-operation.py
@@ -0,0 +1,16 @@
+#List operations
+
+#adding/assigning to lists
+
+# replacing list of characters from start index to end
+s1 = list('Perl')
+print s1
+s1[2:] = list('ar');
+print s1
+
+# replacing list from a given range
+s2 = list('Enginiiring')
+print s2
+s2[5:7] = list('ee')
+# s2[start_including:end_excluding]
+print s2
